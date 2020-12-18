dcf07245c40c0862f38643ce6a165be894fb2490
Dmitry T <takkand.d@gmail.com>
2020-12-18 16:12:34 +0500

    Move to leaflet maps

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import * as GmapVue from 'gmap-vue';
 import Toasted from 'vue-toasted';
 import Clipboard from 'v-clipboard';
 import App from './App.vue';
@@ -8,6 +7,8 @@ import router from './router';
 import store from './store';
 import './scss/custom.scss';
 import './scss/style.scss';
+import 'leaflet/dist/leaflet.css';
+
 import titleMixin from './mixins/titleMixin';
 
 // Install BootstrapVue
@@ -20,12 +21,6 @@ Vue.config.productionTip = false;
 
 Vue.mixin(titleMixin);
 
-Vue.use(GmapVue, {
-  load: {
-    key: process.env.VUE_APP_MAP_API_KEY,
-  },
-  installComponents: true,
-});
 new Vue({
   router,
   store,
--- client/src/views/db/Map.vue
+++ client/src/views/db/Map.vue
@@ -1,64 +1,45 @@
 <template>
   <div>
     <b-card>
-      <GmapMap
-        ref="mmm"
-        :center="center"
-        :zoom="12"
-        map-type-id="terrain"
-        style="width: 100%; height: 50vh"
-      >
-        <GmapMarker v-for="(marker, index) in markers"
-                    :key="index"
-                    :clickable="true"
-                    :draggable="true"
-                    :position="marker.position" @click="center=marker.position">
-        </GmapMarker>
-      </GmapMap>
+      <l-map ref="leafmap"
+             :center="center"
+             :max-bounds="maxBounds"
+             style="height: 500px">
+        <l-tile-layer :url="url"></l-tile-layer>
+        <l-polygon :color="polygon.color" :lat-lngs="polygon.latlngs"></l-polygon>
+      </l-map>
     </b-card>
   </div>
 </template>
 
 <script>
-import { gmapApi } from 'gmap-vue';
+import { latLngBounds } from 'leaflet';
+import { LMap, LPolygon, LTileLayer } from 'vue2-leaflet';
 
 export default {
   name: 'Map',
+  components: {
+    LMap,
+    LTileLayer,
+    LPolygon,
+  },
   data() {
     return {
-      center: {
-        lat: 50,
-        lng: 50,
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      center: [57.2, 65.6],
+      polygon: {
+        latlngs: [
+          [57.1, 65.2],
+          [57.3, 65.4],
+          [57.14, 65.6],
+          [57.1, 65.2],
+        ],
+        color: 'red',
       },
-      markers: [{
-        position: {
-          lat: 10.0,
-          lng: 10.0,
-        },
-      }, {
-        position: {
-          lat: 11.0,
-          lng: 11.0,
-        },
-      }],
     };
   },
-  computed: {
-    google: gmapApi,
-  },
   mounted() {
-    this.fitBounds();
-  },
-  methods: {
-    fitBounds() {
-      this.$refs.mmm.$mapPromise.then((map) => {
-        const bounds = new this.google.maps.LatLngBounds();
-        this.markers.forEach((m) => {
-          bounds.extend(m.position);
-        });
-        map.fitBounds(bounds);
-      });
-    },
+    this.$refs.leafmap.mapObject.fitBounds(latLngBounds(this.polygon.latlngs));
   },
 };
 </script>
