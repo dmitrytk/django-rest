90eb117934250f380382faac22e2ac77082e8cbe
Dmitry T <takkand.d@gmail.com>
2021-03-14 16:30:57 +0500

    Fix server Dockerfile

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -54,14 +54,12 @@ MIDDLEWARE = [
 
 AUTH_USER_MODEL = 'authentication.User'
 
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0:8080',
     'http://localhost:8080',
     'http://0.0.0.0:8000',
     'http://localhost:8000',
-)
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
