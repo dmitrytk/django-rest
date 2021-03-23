386211eb242d3f175caa48d3c3851dc2facef91f
Dmitry <takkand.d@gmail.com>
2021-03-23 19:49:38 +0500

    Add user profile route

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,11 +1,13 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from users.views import me
+
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('me/', me, name='me'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+@api_view()
+def me(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
